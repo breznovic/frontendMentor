43a74ab250531992dbeea5b3466b859caa00e570
breznovic <slymarkel@gmail.com>
2024-02-02 18:25:10 +0300

    add Nutrition

--- src/projects/RecipePage/components/Ingredients/Ingredients.tsx
+++ src/projects/RecipePage/components/Ingredients/Ingredients.tsx
@@ -1,7 +1,31 @@
 import s from "./Ingredients.module.css";
 
 const Ingredients = () => {
-  return <div>Ingredients</div>;
+  return (
+    <div className={s.layout}>
+      <div className={s.title}>Ingredients</div>
+      <div className={s.list}>
+        <ul>
+          <li>
+            <span>2-3 large eggs</span>
+          </li>
+          <li>
+            <span>Salt, to taste</span>
+          </li>
+          <li>
+            <span>Pepper, to taste</span>
+          </li>
+          <li>
+            <span>1 tablespoon of butter or oil</span>
+          </li>
+          <li>
+            <span>optional fillings: cheese, sausages, herbs</span>
+          </li>
+        </ul>
+      </div>
+      <hr className={s.line} />
+    </div>
+  );
 };
 
 export default Ingredients;
--- src/projects/RecipePage/components/Nutrition/Nutrition.tsx
+++ src/projects/RecipePage/components/Nutrition/Nutrition.tsx
@@ -1,7 +1,35 @@
+import { Item } from "./Item/Item";
 import s from "./Nutrition.module.css";
 
+export type ItemType = {
+  title: string;
+  parameter: string;
+};
+
+const items: ItemType[] = [
+  { title: "Calories", parameter: "277Kcal" },
+  { title: "Carbs", parameter: "0g" },
+  { title: "Protein", parameter: "20g" },
+  { title: "Fat", parameter: "22g" },
+];
+
 const Nutrition = () => {
-  return <div>Nutrition</div>;
+  return (
+    <div className={s.layout}>
+      <div className={s.title}>Nutrition</div>
+      <div className={s.text}>
+        The table below shows nutritional values per serving without the
+        additional fillings.
+      </div>
+      {items.map((i) => {
+        return (
+          <div key={i.title}>
+            <Item title={i.title} parameter={i.parameter} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Nutrition;
