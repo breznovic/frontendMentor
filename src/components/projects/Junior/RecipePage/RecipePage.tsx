6d87fe99ca2dc670392445f69c98a4c86b275a5b
breznovic <slymarkel@gmail.com>
2024-02-07 17:56:48 +0300

    fix Recipe project

--- src/components/projects/Junior/RecipePage/RecipePage.tsx
+++ src/components/projects/Junior/RecipePage/RecipePage.tsx
@@ -1,11 +1,22 @@
 import s from "./RecipePage.module.css";
-import Recipe from "./components/Recipe/Recipe";
+import recipeImage from "../../../../assets/images/image-omelette.jpeg";
 
 const RecipePage = () => {
   return (
     <div className={s.main}>
       <div className={s.layout}>
-        <Recipe />
+        <div className={s.recipe}>
+          <img src={recipeImage} />
+          <div className={s.title}>Simple Omelette Recipe</div>
+          <div className={s.subtitle}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </div>
+        </div>
         <div className={s.gridMenu}>
           <div>Preparation time</div>
           <div>Ingredients</div>
