df14fe5cc21314118f62fc8af16796fb8356ce3e
breznovic <slymarkel@gmail.com>
2024-02-10 14:04:44 +0300

    add Final version JobList

--- src/components/projects/Intermediate/JobList/components/VacancyList/VacancyList.tsx
+++ src/components/projects/Intermediate/JobList/components/VacancyList/VacancyList.tsx
@@ -1,16 +1,29 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Vacancy from "../Vacancy/Vacancy";
-import s from "./VacancyList.module.css";
 import vacancies from "../../data.json";
 import Pagination from "../Pagination/Pagination";
 import SearchBar from "../SearchBar/SearchBar";
+import { ActiveTagsContext } from "../../AppContext";
+import s from "./VacancyList.module.css";
 
 const VacancyList = () => {
+  const { activeTags } = useContext(ActiveTagsContext);
+
   const itemsPerPage = 5;
   const [currentPage, setCurrentPage] = useState(1);
   const indexOfLastVacancy = currentPage * itemsPerPage;
   const indexOfFirstVacancy = indexOfLastVacancy - itemsPerPage;
-  const currentVacancies = vacancies.slice(
+
+  const filteredVacancies =
+    activeTags.length > 0
+      ? vacancies.filter((v) =>
+          activeTags.every((tag) =>
+            [...v.languages, ...v.tools, v.role, v.level].includes(tag)
+          )
+        )
+      : vacancies;
+
+  const currentVacancies = filteredVacancies.slice(
     indexOfFirstVacancy,
     indexOfLastVacancy
   );
@@ -20,39 +33,45 @@ const VacancyList = () => {
   };
 
   return (
-    <div>
-      <div>
-        <SearchBar />
+    <>
+      <div className={s.container}>
+        <div className={s.searchBar}>
+          <SearchBar />
+        </div>
+        <div className={s.vacancies}>
+          {currentVacancies.length > 0 ? (
+            currentVacancies.map((v) => (
+              <div key={v.id}>
+                <Vacancy
+                  position={v.position}
+                  company={v.company}
+                  contract={v.contract}
+                  featured={v.featured}
+                  id={v.id}
+                  new={v.new}
+                  role={v.role}
+                  level={v.level}
+                  postedAt={v.postedAt}
+                  location={v.location}
+                  languages={v.languages}
+                  tools={v.tools}
+                />
+              </div>
+            ))
+          ) : (
+            <div>No vacancies found.</div>
+          )}
+        </div>
       </div>
-      <div>
-        {currentVacancies.map((v) => (
-          <div key={v.id}>
-            <Vacancy
-              position={v.position}
-              company={v.company}
-              contract={v.contract}
-              featured={v.featured}
-              id={v.id}
-              new={v.new}
-              role={v.role}
-              level={v.level}
-              postedAt={v.postedAt}
-              location={v.location}
-              languages={v.languages}
-              tools={v.tools}
-            />
-          </div>
-        ))}
-      </div>
-      <div>
+      <div className={s.pagination}>
         <Pagination
           itemsPerPage={itemsPerPage}
-          totalItems={vacancies.length}
+          totalItems={filteredVacancies.length}
           currentPage={currentPage}
           onPageChange={handlePageChange}
         />
       </div>
-    </div>
+    </>
   );
 };
 
